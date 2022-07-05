e2aff2dfe435b5b97e6a7c1a0b2b7484aa95450f
FabianVegaA <fabianveal@gmail.com>
2022-07-05 10:34:14 -0400

    refactor(.): split routes of main func

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,30 @@
 
 module Main where
 
+
 import Data.Monoid (mconcat)
-import Web.Scotty (get, html, param, post, scotty)
+import Web.Scotty (scotty, get, html, post, param)
+import Database.PostgreSQL.Simple
+
+import Lib
+
+localPG :: ConnectInfo
+localPG =
+  defaultConnectInfo
+    { connectHost = "0.0.0.0",
+      connectDatabase = "products",
+      connectUser = "postgres",
+      connectPassword = "123456"
+    }
 
 main :: IO ()
-main = scotty 8080 $ do
+main = do
+  conn <- connect localPG
+
+  routes conn
+
+routes :: Connection -> IO ()
+routes conn = scotty 8080 $ do
   get "/" $ do
     html "Hello, world!"
 
@@ -14,12 +33,9 @@ main = scotty 8080 $ do
     name <- param "name"
     html $ mconcat ["<h1>Hello ", name, "!</h1>"]
 
-  get "/api/product/" $ do
-    html "Get products"
+  get "/api/product/" $ getProducts conn
 
-  get "/api/product/:id" $ do
-    idProduct <- param "id"
-    html $ mconcat ["Get product with id: ", idProduct]
+  get "/api/product/:id" $ getProduct conn
 
-  post "/api/product/" $ do
-    html "Update product and Add new product"
+  post "/api/product/" $ addProduct conn
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,91 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( getProduct,
+    getProducts,
+    createProduct,
+    updateProduct,
+    addProduct,
+  )
+where
+
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson
+import Data.Int
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+import Web.Scotty (ActionM, param, post, status, text)
+import qualified Web.Scotty as S
+
+data Product = Product
+  { idProduct :: Int,
+    name :: String,
+    price :: Int,
+    description :: String
+  }
+  deriving (Show)
+
+instance FromRow Product where
+  fromRow = Product <$> field <*> field <*> field <*> field
+
+instance ToJSON Product where
+  toJSON (Product idProduct name price description) =
+    object
+      [ "id" .= idProduct,
+        "name" .= name,
+        "price" .= price,
+        "description" .= description
+      ]
+
+getProducts :: Connection -> ActionM ()
+getProducts conn = do
+  products <- (liftIO $ query_ conn "SELECT * FROM products") :: ActionM [Product]
+  S.json $ object ["products" .= products]
+
+getProduct :: Connection -> ActionM ()
+getProduct conn = do
+  _idProduct <- param "id" :: ActionM Int
+  let result = query conn "SELECT * FROM products WHERE id = ?" (Only _idProduct)
+  product <- liftIO result :: ActionM [Product]
+  S.json (head product)
+
+addProduct :: Connection -> ActionM ()
+addProduct conn = do
+  _nameProduct <- param "name" :: ActionM String
+
+  hasProductResult <- liftIO $ hasProduct conn _nameProduct
+  if hasProductResult
+    then updateProduct conn
+    else createProduct conn
+
+hasProduct :: Connection -> String -> IO Bool
+hasProduct conn _nameProduct = do
+  n <- execute conn "SELECT * FROM products WHERE name = ?" (Only _nameProduct)
+  return $ n > 0
+
+createProduct :: Connection -> ActionM ()
+createProduct conn = do
+  _name <- param "name" :: ActionM String
+  _price <- param "price" :: ActionM Int
+  _description <- param "description" :: ActionM String
+  let result =
+        execute
+          conn
+          "INSERT INTO products (name, price, description) VALUES (?, ?, ?)"
+          (_name, _price, _description)
+  liftIO result
+
+  S.json $ object ["message" .= ("Product created successfully!" :: String)]
+
+updateProduct :: Connection -> ActionM ()
+updateProduct conn = do
+  _name <- param "name" :: ActionM String
+  _price <- param "price" :: ActionM Int
+  _description <- param "description" :: ActionM String
+  let result =
+        execute
+          conn
+          "UPDATE products SET name = ?, price = ?, description = ? WHERE name = ?"
+          (_name, _price, _description, _name)
+  liftIO result
+  S.json $ object ["message" .= ("Product updated" :: String)]
